1cd6e156e3f917871fa5f81bb8369e5fa298985d
Pedro U <pedroescapalete@gmail.com>
2025-05-23 08:12:50 -0300

    Prova_4_Correção

---
+++ Correção_Prova/exercicio3.js
@@ -0,0 +1,18 @@
+let redeSocial = 1;
+
+switch (redeSocial){
+    case 1: 
+        console.log("Facebook");
+        break;
+
+    case 2:
+        console.log("Instagram");
+        break;
+        
+    case 3:
+        console.log("Twitter");
+        break;
+        
+    default:
+        console.log("Rede não encontrada.")    
+}
