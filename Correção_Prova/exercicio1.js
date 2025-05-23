56d2080d77db9c2f23b4c6e0b18351856829e0bb
Pedro U <pedroescapalete@gmail.com>
2025-05-23 08:07:30 -0300

    Prova_4_Correção

---
+++ Correção_Prova/exercicio1.js
@@ -0,0 +1,8 @@
+const orcamentoViagem = 4000;
+let dinheiroAtual = 2000;
+
+let quantoFalta = orcamentoViagem - dinheiroAtual
+console.log("Falta: ", quantoFalta);
+
+
+//Pedro Urbano
---
+++ Correção_Prova/exercicio2.js
@@ -0,0 +1,11 @@
+let pontuacao = 30;
+
+if (pontuacao < 50){
+    console.log("Eliminado");
+} else if (pontuacao >= 50 && pontuacao >= 79){
+    console.log("Classificado");
+} else {
+    console.log("Finalista");
+}
+
+//Pedro Urbano
