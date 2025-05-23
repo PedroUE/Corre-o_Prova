e06102a1dd3425511f4e246087bcce83dd68fe50
Pedro U <pedroescapalete@gmail.com>
2025-05-23 08:23:05 -0300

    Prova_4_Correção

---
+++ Correção_Prova/exercicio4.js
@@ -0,0 +1,9 @@
+let curtidasPosts = [1, 5, 2, 4, 2];
+let soma = 0;
+
+for(let i = 0; i < curtidasPosts.length ; i++){
+    soma += curtidasPosts[i];
+
+}
+
+console.log("Total de curtidas: ", soma );
