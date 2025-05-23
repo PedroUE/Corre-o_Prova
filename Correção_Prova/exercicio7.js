c3d837ed5ebdf9f9fccbbee45714773693933476
Pedro U <pedroescapalete@gmail.com>
2025-05-23 08:39:53 -0300

    Prova_4_Correção

---
+++ Correção_Prova/exercicio7.js
@@ -0,0 +1,19 @@
+let opcao = 2;
+
+switch (opcao){
+    case 1:
+        console.log("Cheesburguer  R$ 12");
+        break;
+
+    case 2: 
+        console.log("X-Salada   R$ 17");
+        break;
+    
+    case 3:
+        console.log("Veggie Burger    R$ 15");
+        break;
+        
+        
+    default:
+        console.log("Opção Invalida");
+}
